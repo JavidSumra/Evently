6d2f956f7badc7e5bc92c61a25983b3de53f84b0
JavidSumra <javidsumra135@gmail.com>
2024-02-24 14:04:57 +0530

    Updating dev command to fix deployment

--- Backend/server.js
+++ Backend/server.js
@@ -6,17 +6,13 @@ const PORT = process.env.PORT || 3008;
 
 connectDB()
   .then(() => {
-    try {
-      app.listen(PORT, () => {
-        console.log(`Server Running on PORT:${PORT}`);
-        // simulate a ready application after 1 second
-        setTimeout(function () {
-          process.send("ready");
-        }, 4000);
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    app.listen(PORT, () => {
+      console.log(`Server Running on PORT:${PORT}`);
+      // simulate a ready application after 4 second
+      setTimeout(function () {
+        process.send("ready");
+      }, 4000);
+    });
   })
   .catch((err) => {
     console.log("Connection With DB Failed", err);
