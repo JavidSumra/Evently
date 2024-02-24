68ca42d46e80ed64c4cd9e360b5d6b45f8c98090
JavidSumra <javidsumra135@gmail.com>
2024-02-24 12:37:35 +0530

    Updating dev Command to Fix pm2 Issue

--- Backend/server.js
+++ Backend/server.js
@@ -6,13 +6,17 @@ const PORT = process.env.PORT || 3008;
 
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server Running on PORT:${PORT}`);
-      // simulate a ready application after 1 second
-      setTimeout(function () {
-        process.send("ready");
-      }, 1000);
-    });
+    try {
+      app.listen(PORT, () => {
+        console.log(`Server Running on PORT:${PORT}`);
+        // simulate a ready application after 1 second
+        setTimeout(function () {
+          process.send("ready");
+        }, 4000);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   })
   .catch((err) => {
     console.log("Connection With DB Failed", err);
