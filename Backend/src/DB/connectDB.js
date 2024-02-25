c997d2362992df9a3047ea70d2e958b6209ec5cc
JavidSumra <javidsumra135@gmail.com>
2024-02-25 16:50:41 +0530

    Updating DB Connection Settings

--- Backend/src/DB/connectDB.js
+++ Backend/src/DB/connectDB.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
+const ENV = process.env.NODE_ENV || "development";
 
-const DB_NAME = "Evently";
+const URL =
+  ENV != "development"
+    ? process.env.MONGO_DB_URL
+    : "mongodb://127.0.0.1:27017/Evently";
 
 const connectDB = async () => {
   try {
-    const connectionInstance =
-      await mongoose.connect(`${process.env.MONGO_DB_URL}${DB_NAME}
-      `);
+    const connectionInstance = await mongoose.connect(URL);
 
     console.log(
       "Successfully Connected to Mongo DB at Host:",
