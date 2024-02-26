96507879dbf2c378e234486f75a436e3097c40da
JavidSumra <javidsumra135@gmail.com>
2024-02-26 08:59:46 +0530

    Updating dev-dist

--- Evently/dev-dist/sw.js
+++ Evently/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lf37e03g0k8"
+    "revision": "0.qt9fno4tq68"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
