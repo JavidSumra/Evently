3c03d778430bbe6f3fea97c847d8052e5d94c391
JavidSumra <javidsumra135@gmail.com>
2024-03-06 20:10:36 +0530

    Updating bcrypt module for linux in docker

--- Backend/src/models/User.model.js
+++ Backend/src/models/User.model.js
@@ -1,6 +1,6 @@
 "use strict";
 import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
