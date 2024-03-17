45b7ead7cc7051a9fa97dd7fb6eb768f82bf20b3
JavidSumra <javidsumra135@gmail.com>
2024-03-17 10:47:13 +0530

    Adding Sentry

--- Evently/src/constant/constant.ts
+++ Evently/src/constant/constant.ts
@@ -2,3 +2,4 @@ export const API_ENDPOINT =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3008/api/v1"
     : "https://evently-backend-jiec.onrender.com/api/v1";
+export const SENTRY_DNS = import.meta.env.VITE_SENTRY_DNS;
--- Evently/src/vite-env.d.ts
+++ Evently/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   [x: string]: any;
   readonly VITE_API_ENDPOINT: string;
+  readonly VITE_SENTRY_DNS: string;
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
