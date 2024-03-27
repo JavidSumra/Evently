a1c11a8a56a0a8c7cc6b3dbc397cc3531cb63104
JavidSumra <javidsumra135@gmail.com>
2024-03-27 23:05:16 +0530

    Fixing Issue of .env

--- Evently/dev-dist/sw.js
+++ Evently/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0b3vg89lrl8"
+    "revision": "0.5v8c9e2jf9o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
